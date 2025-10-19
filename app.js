2afaadfdbe66dcda8175c669121c3dc219ec905f
听风 <3086099731@qq.com>
2025-10-19 14:51:20 +0800

    前端部署完毕开始启动后端云环署部署

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 App({
-  onLaunch() {},
+  onLaunch() {
+    // 初始化云开发环境
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+    } else {
+      wx.cloud.init({
+        env: 'cloud1-7g0bpzkg04df43f9', // 云环境ID
+        traceUser: true,
+      });
+    }
+  },
+  
+  globalData: {
+    userInfo: null,
+    openid: null
+  }
 });
 
